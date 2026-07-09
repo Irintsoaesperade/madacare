34621afd10da4ed9a51387edcd2bf0f7a5cf8ee7
Esperade <irintsoaesperade@gmail.com>
2026-07-09 13:17:03 +0300

    Configuration des migrations TypeORM

---
+++ backend/src/migrations/1783591619333-InitialDatabase.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialDatabase1783591619333 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
